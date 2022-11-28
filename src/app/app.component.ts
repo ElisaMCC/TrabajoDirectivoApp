e2565153d88b631234a3c1320bd9dba1a8a77f79
ElisaMCC <elisamargaritacimacaamal@gmail.com>
2022-11-27 20:22:57 -0500

    Directivas

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = 'Registro de Usuarios';
+  mensaje= "";
+  registrado=false;
+  nombre:string="";
+  apellido:string="";
+  cargo:string="";
+  entradas: object[
+    
+  ];
+
+  constructor(){
+    this.entradas=[
+      {titulo:"Python cada dia mas presente"},
+      {titulo:"Java presente desde hace mas de 20 años"},
+      {titulo:"JavaScript cada dia mas funcional"},
+      {titulo:"Kotlin potencia pata tus apps"},
+      {titulo:"¿Donde quedo pascal?"},
+    ]
+
+  }
+
+  registrarUsuario(){
+    this.registrado=true
+    this.mensaje="usuario registrado con exito"
+  }
+}
